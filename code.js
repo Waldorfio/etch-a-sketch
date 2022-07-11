07035ed68943edf5627653a2994555a52b2aea62
Waldorfio <walid.hanifi707@gmail.com>
2022-07-11 14:57:33 +1000

    Add successfully loop for creating multiple classes

--- code.js
+++ code.js
@@ -1,20 +1,13 @@
 const container = document.querySelector('#container');
 
 
-const div = document.createElement('div');
-div.setAttribute("id", "Div1")
+for (i=1; i<30+1; i++) {
+    squareClass = document.createElement('div');
+    squareClass.className = 'square';
+    squareClass.innerHTML = 'a';
+    document.getElementById('container').appendChild(squareClass);
+}
 
-// console.log(container.outerHTML);
-// for (i=1; i<30+1; i++) {
-//     content.classList.add('square' + i);
-// }
-// content.classList.add('square1');
-// content.square1.innerHTML = 'text test'
-
-// div.classList.add('square1');
-
-// div.getElementsByClassName('square1').innerHTML = 'free';
-document.getElementById('Div1').innerHTML = 'free';
-
+console.log('-----');
 console.log(container.outerHTML);
-console.log(div.outerHTML);
+// console.log(div.outerHTML);
