3a4747dc916f9f006ea6ceca202794dcc8a4d76c
Waldorfio <walid.hanifi707@gmail.com>
2022-07-12 19:26:23 +1000

    Add interactive control buttons

--- code.js
+++ code.js
@@ -1,27 +1,44 @@
 const container = document.querySelector('#container');
+let noSquares = 256;
 
-const noSquares = 256;
+userInp = document.createElement('button');
+userInp.className = 'user-input';
+userInp.innerHTML = 'GRID SIZE';
+userInp.addEventListener('click', userPrompt);
+document.getElementById('container').appendChild(userInp);
 
-function squareClicked(squareClass, clicked) {
-    // if (clicked == 0) {inverseColour = 'red'};
-    // if (clicked == 0) {inverseColour = 'red'};
+reset = document.createElement('button');
+reset.className = 'reset';
+reset.innerHTML = 'RESET';
+reset.addEventListener('click', resetGrid);
+document.getElementById('container').appendChild(reset);
+
+function resetGrid() {
+    for (i=0; i<(noSquares); i++) {
+        currentSquare = document.getElementsByClassName('square')[i];
+        currentSquare.style['background-color'] = 'grey';
+    }
+}
+
+function userPrompt() {
+    noSquares = prompt('Please enter the grid size (1d only):');
+    resetGrid();
+    return noSquares
+}
+
+function squareClicked(squareClass) {
     squareClass.style['background-color'] = 'red';
-    // inverseColour = 
-    // clicked = 0;
 }
 
-function alertFunc(i) {
-    alert('id: ' + i);
-}
-
+// Creating the tile classes, and assigning the event listener
 for (i=1; i<(noSquares+1); i++) {
     squareClass = document.createElement('div');
     squareClass.className = 'square';
-    squareClass.innerHTML = 'a';
-    // squareClass.addEventListener('click', () => alertFunc(i));       // original attempt at adding eventlisteners per class
-    squareClass.addEventListener('click', alertFunc.bind(this,i));      // new proper way of "binding" eventlistener to the individual iteration.
+    squareClass.innerHTML = '&nbsp';    // Displays empty spaces
+    squareClass.addEventListener('mouseenter', squareClicked.bind(this,squareClass));      // new proper way of "binding" eventlistener to the individual iteration.
 
-    document.getElementById('container').appendChild(squareClass);
+
+    document.getElementById('container').appendChild(squareClass);      // Appends the child to the parent div
 }
 
 
