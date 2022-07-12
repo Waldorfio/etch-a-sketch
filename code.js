fcc37960b3dd7137e5fe5864fb73814e25f8a77a
Waldorfio <walid.hanifi707@gmail.com>
2022-07-12 18:14:00 +1000

    Add bind to each class created via loop

--- code.js
+++ code.js
@@ -1,13 +1,30 @@
 const container = document.querySelector('#container');
 
+const noSquares = 256;
 
-for (i=1; i<30+1; i++) {
+function squareClicked(squareClass, clicked) {
+    // if (clicked == 0) {inverseColour = 'red'};
+    // if (clicked == 0) {inverseColour = 'red'};
+    squareClass.style['background-color'] = 'red';
+    // inverseColour = 
+    // clicked = 0;
+}
+
+function alertFunc(i) {
+    alert('id: ' + i);
+}
+
+for (i=1; i<(noSquares+1); i++) {
     squareClass = document.createElement('div');
     squareClass.className = 'square';
     squareClass.innerHTML = 'a';
+    // squareClass.addEventListener('click', () => alertFunc(i));       // original attempt at adding eventlisteners per class
+    squareClass.addEventListener('click', alertFunc.bind(this,i));      // new proper way of "binding" eventlistener to the individual iteration.
+
     document.getElementById('container').appendChild(squareClass);
 }
 
+
 console.log('-----');
-console.log(container.outerHTML);
+// console.log(container.outerHTML);
 // console.log(div.outerHTML);
